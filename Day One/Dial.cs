26e269d552ec2963c435d46305ef006d944933d7
Sven Kribitz <Sven.Kribitz@Outlook.de>
2025-12-02 23:06:57 +0100

    Solved Day One

---
+++ Day One/Dial.cs
@@ -0,0 +1,44 @@
+﻿namespace Day_One;
+
+public class Dial
+{
+    /// <summary>
+    /// Current position of the dial. If it hits above 99, it should go down to 0 and vice versa.
+    /// </summary>
+    public int CurrentPosition { get; private set; } = 50;
+
+    /// <summary>
+    /// Turns the dial in the given direction and given number.
+    /// </summary>
+    /// <param name="pos">A string containing the direction (R or L) and the number to turn the dial by.</param>
+    /// <example><code>TurnDialByPosition("R20");</code> is R20, the dial is to be turned 20 to the right.</example>
+    public void TurnDialByPosition(string pos)
+    {
+        string direction = pos[0].ToString().ToUpper();
+        int distance = int.Parse(pos.Substring(1));
+        switch (direction)
+        {
+            case "R":
+                TurnDial(distance);
+                break;
+            case "L":
+                TurnDial(-distance);
+                break;
+            default:
+                throw new InvalidDataException("Invalid direction");
+        }
+    }
+    /// <summary>
+    /// Turns the dial by the given position
+    /// </summary>
+    /// <param name="pos"></param>
+    private void TurnDial(int pos)
+    {
+        // C# does not use 'true' modulo as it does not truncate into negative thus needing another formular. 
+        // ((value % divisor) + divisor) % divisor
+        // Meaning the value does get pushed into the positives by adding the devisor. If it is above the devisor, it gets truncated back into below the devisor.
+        int newPosition = ((CurrentPosition + pos) % 100 + 100) % 100;
+        CurrentPosition = newPosition;
+        Console.WriteLine("Dialed to {0}", CurrentPosition);
+    }
+}
--- Day One/Program.cs
+++ Day One/Program.cs
@@ -4,5 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
+        // string[] puzzleInput =
+        // {
+        //     "L68",
+        //     "L30",
+        //     "R48",
+        //     "L5",
+        //     "R60",
+        //     "L55",
+        //     "L1",
+        //     "L99",
+        //     "R14",
+        //     "L82"
+        // };
+        string[] puzzleInput = File.ReadAllLines("input.txt");
+        Dial dial = new Dial();
+        int dialReachedZero = 0;
+        foreach (string input in puzzleInput)
+        {
+            dial.TurnDialByPosition(input);
+            if (dial.CurrentPosition == 0)
+            {
+                dialReachedZero++;
+                Console.WriteLine("Dial reached zero");
+            }
+        }
+        Console.WriteLine(dialReachedZero);
     }
 }
